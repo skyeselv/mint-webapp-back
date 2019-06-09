567efb178a7bc828de6067eb4404da530dba30ed
Selishchev Ivan <selishcheviv@gmail.com>
2019-06-09 18:57:30 +0300

    Initial commit

---
+++ server.js
@@ -0,0 +1,15 @@
+var express = require("express")
+var cors = require("cors")
+var bodyParser = require("body-parser")
+var app = express()
+var port = process.env.PORT || 5000
+
+app.use(bodyParser.json())
+app.use(cors())
+app.use(bodyParser.urlencoded({extended: false}))
+
+var Users = require("./routes/users")
+
+app.use("/users", Users)
+
+app.listen(port, () => {console.log("Server running on port: " + port)})
